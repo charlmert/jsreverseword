f283ee6a6f05c5209eebfa98d1fa5a061ce1afdf
Charl Joseph Mert <charl.mert@gmail.com>
2016-03-07 18:58:55 +0200

    preserving dots and uppercase

--- js/util.js
+++ js/util.js
@@ -3,19 +3,37 @@
  * of the original sentence
  */
 function reverseWords(str) {
-	parts = str.split(/\W/);
+	parts = str.split(' ');
 	ret = '';
 	for (i = 0; (i < parts.length); i++) {
+		// Preserving uppercase for the word if any upper case chars are matched
 		hasUpper = parts[i].match(/^[A-Z]/);
+		hasDot = parts[i].match(/\./);
+
 		for (x = parts[i].length - 1; (x > -1); x--) {
-			if (hasUpper != null && i == parts[i].length - 1) {
+			if (hasUpper != null && x == parts[i].length - 1) {
 				ret += parts[i][x].toUpperCase();
 				continue;
 			}
 
-			ret += parts[i][x].toUpperCase();
+			if (hasDot != null && x == 0) {
+				ret += '.';
+				continue;
+			}
+
+			if (hasDot != null && x > 0) {
+				ret += parts[i][x-1];
+				continue;
+			}
+
+			ret += parts[i][x].toLowerCase();
 		}
-		ret += ' ';
+		
+		// Made test pass
+		if (i < parts.length - 1) {
+			ret += ' ';
+		}
 	}
+
 	return ret;
 }
